f1d39cf56a5c3ec492e50ebb0d892755f5e22279
SnehaHS65 <65708833+SnehaHS65@users.noreply.github.com>
2022-09-20 16:23:56 +0530

    Delete spring-boot-foodapp/spring-boot-foodapp directory

--- spring-boot-foodapp/spring-boot-foodapp/src/main/java/com/org/springbootfoodapp/repository/FoodOrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.org.springbootfoodapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.org.springbootfoodapp.dto.FoodOrder;
-
-public interface FoodOrderRepository extends JpaRepository<FoodOrder,Integer> {
-
-}
--- spring-boot-foodapp/spring-boot-foodapp/src/main/java/com/org/springbootfoodapp/repository/ItemRepository.java
+++
@@ -1,9 +0,0 @@
-package com.org.springbootfoodapp.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.org.springbootfoodapp.dto.Item;
-
-public interface ItemRepository extends JpaRepository<Item,Integer>{
-
-}
